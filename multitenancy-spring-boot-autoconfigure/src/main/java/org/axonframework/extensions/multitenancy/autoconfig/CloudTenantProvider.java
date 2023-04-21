225e29e5bcd3f6dfc99fb0cfa3cd6cf927faaa3d
Gerard Klijs <gerard.klijs@axoniq.io>
2023-04-21 13:51:20 +0200

    Clean up CloudTenantProvider.

--- multitenancy-spring-boot-autoconfigure/src/main/java/org/axonframework/extensions/multitenancy/autoconfig/CloudTenantProvider.java
+++ multitenancy-spring-boot-autoconfigure/src/main/java/org/axonframework/extensions/multitenancy/autoconfig/CloudTenantProvider.java
@@ -16,13 +16,10 @@
 
 package org.axonframework.extensions.multitenancy.autoconfig;
 
-import io.axoniq.axonserver.grpc.admin.ContextOverview;
-import io.axoniq.axonserver.grpc.admin.ContextUpdate;
 import org.axonframework.axonserver.connector.AxonServerConnectionManager;
 import org.axonframework.common.Registration;
 import org.axonframework.common.StringUtils;
 import org.axonframework.extensions.multitenancy.components.MultiTenantAwareComponent;
-import org.axonframework.extensions.multitenancy.components.TenantConnectPredicate;
 import org.axonframework.extensions.multitenancy.components.TenantDescriptor;
 import org.axonframework.extensions.multitenancy.components.TenantProvider;
 import org.axonframework.lifecycle.Lifecycle;
@@ -54,25 +51,19 @@ public class CloudTenantProvider implements TenantProvider, Lifecycle {
 
     private final Set<TenantDescriptor> tenantDescriptors = new HashSet<>();
     private final String preDefinedContexts;
-    private final TenantConnectPredicate tenantConnectPredicate;
     private final AxonServerConnectionManager axonServerConnectionManager;
     private ConcurrentHashMap<TenantDescriptor, List<Registration>> registrationMap = new ConcurrentHashMap<>();
 
     public CloudTenantProvider(String preDefinedContexts,
-                               TenantConnectPredicate tenantConnectPredicate,
                                //restTemplate
                                AxonServerConnectionManager axonServerConnectionManager) {
         this.preDefinedContexts = preDefinedContexts;
-        this.tenantConnectPredicate = tenantConnectPredicate;
         this.axonServerConnectionManager = axonServerConnectionManager;
     }
 
     public void start() {
         tenantDescriptors.addAll(getInitialTenants());
         tenantDescriptors.forEach(this::addTenant);
-        if (preDefinedContexts == null || preDefinedContexts.isEmpty()) {
-            subscribeToUpdates();
-        }
     }
 
     public List<TenantDescriptor> getInitialTenants() {
@@ -83,8 +74,6 @@ public class CloudTenantProvider implements TenantProvider, Lifecycle {
                                        .map(String::trim)
                                        .map(TenantDescriptor::tenantWithId)
                                        .collect(Collectors.toList());
-            } else {
-                initialTenants = getTenantsAPI();
             }
         } catch (Exception e) {
             logger.error("Error while getting initial tenants", e);
@@ -92,93 +81,9 @@ public class CloudTenantProvider implements TenantProvider, Lifecycle {
         return initialTenants;
     }
 
-    private void subscribeToUpdates() {
-        //how to subscribe from java to SSE, server sent events (/api/space/{spaceId}/context/updates)
-        //      try {
-        //call -> /api/space/{spaceId}/context/updates
-        //get space id from context name, split by @ second part is spaceId
-//            ResultStream<ContextUpdate> contextUpdatesStream = axonServerConnectionManager
-//                    .getConnection(ADMIN_CTX)
-//                    .adminChannel()
-//                    .subscribeToContextUpdates();
-
-
-//            contextUpdatesStream.onAvailable(() -> {
-
-        //     THIS PART IS THE SAME no need to change
-//                try {
-//                    ContextUpdate contextUpdate = contextUpdatesStream.nextIfAvailable();
-//                    if (contextUpdate != null) {
-//                        switch (contextUpdate.getType()) {
-//                            case CREATED:
-//                                handleContextCreated(contextUpdate);
-//                                break;
-//                            case DELETED:
-//                                removeTenant(TenantDescriptor.tenantWithId(contextUpdate.getContext()));
-//                        }
-//                    }
-//                } catch (Exception e) {
-//                    logger.error(e.getMessage(), e);
-//                }
-//            });
-//        } catch (Exception e) {
-//            logger.error("Error while subscribing to context updates", e);
-//        }
-    }
-
-    private void handleContextCreated(ContextUpdate contextUpdate) {
-        try {
-            ContextOverview contextOverview = null; //call /api/space/{spaceId}/context/{id}
-            TenantDescriptor newTenant = toTenantDescriptor(contextOverview);
-            if (tenantConnectPredicate.test(newTenant) && !tenantDescriptors.contains(newTenant)) {
-                addTenant(newTenant);
-            }
-        } catch (Exception e) {
-            logger.error(e.getMessage(), e);
-        }
-    }
-
     @Override
     public List<TenantDescriptor> getTenants() {
         return new ArrayList<>(tenantDescriptors);
-    }
-
-
-    private List<TenantDescriptor> getTenantsAPI() {
-        //call rest endpoint /api/space/{spaceId}/context and return list of TenantDescriptors
-
-        //this you will not need
-//        return axonServerConnectionManager.getConnection(ADMIN_CTX)
-//                                          .adminChannel()
-//                                          .getAllContexts()
-//                                          .join()
-//                                          .stream()
-
-        //this you will need
-//                                          .map(this::toTenantDescriptor)
-//                                          .filter(tenantConnectPredicate)
-//                                          .collect(Collectors.toList());
-        return null;
-    }
-
-    private TenantDescriptor toTenantDescriptor(ContextOverview context) {
-
-        //put to meta data ->
-//        "contextId": "string",
-//                "contextName": "string",
-//                "spaceId": "string",
-//                "type": "string",
-//                "free": true,
-//                "contextStatus": "string",
-//                "region": "string",
-//                "creationTime": "2022-07-15T10:16:37.710Z",
-//                "scheduledCleanupTime": "2022-07-15T10:16:37.710Z",
-//                "clusterId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-//                "contextClusterType": "string"
-
-        return new TenantDescriptor(context.getName(),
-                                    context.getMetaDataMap(), // <---------
-                                    "");
     }
 
     protected void addTenant(TenantDescriptor tenantDescriptor) {
@@ -187,16 +92,6 @@ public class CloudTenantProvider implements TenantProvider, Lifecycle {
                 .forEach(bus -> registrationMap
                         .computeIfAbsent(tenantDescriptor, t -> new CopyOnWriteArrayList<>())
                         .add(bus.registerAndStartTenant(tenantDescriptor)));
-    }
-
-    protected void removeTenant(TenantDescriptor tenantDescriptor) {
-        if (tenantDescriptors.contains(tenantDescriptor) && tenantDescriptors.remove(tenantDescriptor)) {
-            List<Registration> registrations = registrationMap.remove(tenantDescriptor);
-            if (registrations != null && !registrations.isEmpty()) {
-                registrations.forEach(Registration::cancel);
-            }
-            axonServerConnectionManager.disconnect(tenantDescriptor.tenantId());
-        }
     }
 
     @Override
