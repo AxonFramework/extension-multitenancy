f9a55b68b9f3922d16edf5c61fc0e20cb13bafde
Stefan <91stefan@gmail.com>
2023-11-22 09:57:28 +0100

    add a comment about tenant segment creation, and a shutdown hook in tenant provided

--- multitenancy-spring-boot-autoconfigure/src/main/java/org/axonframework/extensions/multitenancy/autoconfig/AxonServerTenantProvider.java
+++ multitenancy-spring-boot-autoconfigure/src/main/java/org/axonframework/extensions/multitenancy/autoconfig/AxonServerTenantProvider.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -215,5 +215,12 @@ public class AxonServerTenantProvider implements TenantProvider, Lifecycle {
     @Override
     public void registerLifecycleHandlers(@Nonnull LifecycleRegistry lifecycle) {
         lifecycle.onStart(Phase.INSTRUCTION_COMPONENTS + 10, this::start);
+        lifecycle.onShutdown(Phase.INSTRUCTION_COMPONENTS + 10, this::shutdown);
+    }
+
+    private void shutdown() {
+        registrationMap.forEach((tenant, registrationList) -> {
+            registrationList.forEach(Registration::cancel);
+        });
     }
 }
--- multitenancy/src/main/java/org/axonframework/extensions/multitenancy/components/scheduling/TenantEventSchedulerSegmentFactory.java
+++ multitenancy/src/main/java/org/axonframework/extensions/multitenancy/components/scheduling/TenantEventSchedulerSegmentFactory.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -22,6 +22,7 @@ import java.util.function.Function;
 
 /**
  * Factory for creating {@link EventScheduler} segments for a given {@link TenantDescriptor}.
+ * After a segment is created, it may be started automatically by the factory.
  *
  * @author Stefan Dragisic
  * @since 4.9.0
