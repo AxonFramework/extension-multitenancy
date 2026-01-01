6c0a2d2827a6e84d3f7d9e71b7d7befa6c56f696
Theo Emanuelsson <skurutheo@gmail.com>
2026-01-01 06:17:00 +0100

    Add MultiTenantEventProcessorPredicate for configuration
    Simple predicate interface to determine if an event processor should
    be multi-tenant. Portable as-is from AF4.
    
    Note: MultiTenantEventProcessingModule requires complete redesign for
    AF5's new configuration architecture and is deferred to Spring Boot
    autoconfigure module.

---
+++ multitenancy/src/main/java/org/axonframework/extensions/multitenancy/configuration/MultiTenantEventProcessorPredicate.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2010-2025. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.axonframework.extensions.multitenancy.configuration;
+
+import java.util.function.Predicate;
+
+/**
+ * Represents a predicate to determine if an event processor should be multi-tenant.
+ * <p>
+ * This interface extends {@link Predicate<String>} and is used to test whether a given event processor should be
+ * considered as multi-tenant. The input to the predicate is the name of the event processor.
+ *
+ * @author Stefan Dragisic
+ * @since 5.0.0
+ */
+public interface MultiTenantEventProcessorPredicate extends Predicate<String> {
+
+    /**
+     * A {@link MultiTenantEventProcessorPredicate} resulting in {@code true} for any processor name.
+     *
+     * @return A {@link MultiTenantEventProcessorPredicate} resulting in {@code true} for any processor name.
+     */
+    static MultiTenantEventProcessorPredicate enableMultiTenancy() {
+        return name -> true;
+    }
+
+    /**
+     * A {@link MultiTenantEventProcessorPredicate} resulting in {@code false} for any processor name.
+     *
+     * @return A {@link MultiTenantEventProcessorPredicate} resulting in {@code false} for any processor name.
+     */
+    static MultiTenantEventProcessorPredicate disableMultiTenancy() {
+        return name -> false;
+    }
+}
